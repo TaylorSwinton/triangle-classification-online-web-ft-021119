1cf9c98d988415b4e2501fec4c6e33ed757b1af5
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-28 22:35:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,9 +22,9 @@ class Triangle
       if lengths_in_order[0] == lengths_in_order[1] && lengths_in_order[1] == lengths_in_order[2]
         :equilateral
       elsif lengths_in_order[0] != lengths_in_order[1] && lengths_in_order[1] != lengths_in_order[2]
-
+        :scalene
       else
-
+        :isosceles
       end
     end
 
