8f1a1f4edfff57c788251f5e403f8d17bada9369
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-28 21:46:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,11 @@ class Triangle
     sum_of_lengths = @length_1 + @length_2 + @length_3
     two_of_lengths = @length_1 + @length_2
     if sum_of_lengths == 0 || two_of_lengths <= @length_3
-
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
     else
 
     end
